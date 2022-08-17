e039893763ecc9fa53a584d10a0b7197d1b56925
yomark14 <39974565+yomark14@users.noreply.github.com>
2022-08-16 21:32:10 -0500

    cambios de referencias

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 //let usuarios = [{nombre:"Mali",saldo=200, password: "A1234"},{nombre="Gera", saldo=290, password: "B4321"}]
-let numero, operador;
+let contrasena, operador;
 
 function CapturarDatos(id){
-    //variables locales
-    let Imppassword = document.getElementById("'contrasena'");
+    let Imppassword = document.getElementById('cont'); 
+}
 
-    if(id == "Aceptar"||id == "Borrar"||id == "Cancelar")
+  
+   //contrasena = parseInt(document.getElementsByClassName("contrasena")[0].value);
+    /*if(id == "Aceptar"||id == "Borrar"||id == "Cancelar")
     {
   
     }else{
-        numero = (id);
-        Imppassword.innerText = numero;
-    }
-}
+        contrasena = document.getElementsByName("contrasena")[0].value + numero;
+    }*/
